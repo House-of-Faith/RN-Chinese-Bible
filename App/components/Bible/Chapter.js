34fb3b9313942ff235c917ade8ad7c0dbc872ec2
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2020-08-30 18:31:29 -0600

    add key to Chapter verses

--- App/components/Bible/Chapter.js
+++ App/components/Bible/Chapter.js
@@ -3,5 +3,5 @@ import React from 'react';
 import Verse from './Verse';
 
 export default function Chapter({ verses }) {
-	return verses?.map((verse, i) => <Verse number={i + 1} text={verse} />);
+	return verses?.map((verse, i) => <Verse key={i} number={i + 1} text={verse} />);
 }
