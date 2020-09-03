bfab99cac28f1e85f1a41f335791951fec29ba14
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2020-09-02 20:21:22 -0600

    prevent uneeded dispatches

--- App/RootContainer.js
+++ App/RootContainer.js
@@ -3,13 +3,15 @@
 The purpose of this component is to place any app level logic that needs to happen
 
 */
-import React from "react";
-import { View } from "react-native";
-import { useSelector, useDispatch } from "react-redux";
-import { usePersistStore } from "lib/hooks";
-import { selectors } from "store";
+import React from 'react';
+import { View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { usePersistStore } from 'lib/hooks';
+import { selectors } from 'store';
 export default function RootContainer({ children }) {
     const storeRehydrated = useSelector(selectors.storeRehydrated);
+    const showDropdown = useSelector(selectors.showDropdown);
+
     const dispatch = useDispatch();
     usePersistStore();
     if (!storeRehydrated) return null;
@@ -17,8 +19,9 @@ export default function RootContainer({ children }) {
         <View
             style={{ flex: 1 }}
             onStartShouldSetResponder={() => {
-                dispatch({ type: "HIDE_DROPDOWN" });
-                return false;
+              if (!showDropdown) return;
+              dispatch({ type: 'HIDE_DROPDOWN' });
+              return false;
             }}
         >
             {children}
