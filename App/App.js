00a225927a7494d5c0258ede63f5b2b167706223
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2020-09-09 18:31:16 -0600

    ignore debugger warnings

--- App/App.js
+++ App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { registerRootComponent } from 'expo';
 
@@ -8,6 +9,15 @@ import RootNavigation from 'navigation/RootNavigation';
 import RootContainer from './RootContainer';
 import Bugsnag from 'lib/Bugsnag';
 Bugsnag;
+
+// Use this with caution!!
+// Please try to resolve the warning before ignoring it here
+YellowBox.ignoreWarnings([
+  // Warning displays only when debugger is running
+  'Remote debugger is in a background tab',
+  // Bugsnag warning displays only when debugger is running
+  'This call to Bugsnag',
+]);
 
 const App = () => {
   return (
